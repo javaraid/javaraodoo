61c12e04c53c4816e28bef3b348b6721b803c0e0
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2019-07-24 08:22:22 +0700

    [FIX] product_merge

--- product_merge/models/__init__.py
+++ product_merge/models/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import ir_sequence
+from . import product
