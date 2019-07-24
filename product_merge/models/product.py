8e608352d0f99005a68c9e01c8cd4ecda8a5f0a3
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2019-07-24 08:44:13 +0700

    [FIX] product_merge: add log

--- product_merge/models/product.py
+++ product_merge/models/product.py
@@ -20,13 +20,15 @@ class ProductProduct(models.Model):
             # find original variant from its template
             tmpl = prod_to_del.product_tmpl_id
             prod_to_merge = [
-                prod for prod in tmpl.product_variant_ids if prod.id != prod_to_del.id][0]
+                prod for prod in tmpl.product_variant_ids if prod.id != prod_to_del.id]
+            if not prod_to_merge:
+                _logger.error('\n==== PRODUCT NAME: %S ====' % prod_to_del.name)
             # set not available in pos
             self.env.cr.execute(
                 'UPDATE product_template SET available_in_pos=null where id=%i' % prod_to_del.product_tmpl_id.id)
             # merge product in pos order line
             self.env.cr.execute(
-                'UPDATE pos_order_line SET product_id=%i where id=%i' % (prod_to_merge.id, prod_to_del.id,))
+                'UPDATE pos_order_line SET product_id=%i where id=%i' % (prod_to_merge[0].id, prod_to_del.id,))
             loop += 1
             progress = loop / total * 100
-            _logger.error('==== MERGE POS ORDER LINE PROGRESS: %i %% ====' % int(progress))
+            _logger.error('\n==== MERGE POS ORDER LINE PROGRESS: %i %% ====' % int(progress))
