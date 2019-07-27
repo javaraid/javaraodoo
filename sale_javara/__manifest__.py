ba34079d9d5b5ec8d1fac98a65322d5ef3cc4672
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2019-07-27 20:38:51 +0700

    [ADD] init sale_javara

---
+++ sale_javara/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Sale Javara',
+    'summary': 'Custom Sale Management for Javara',
+    'license': 'AGPL-3',
+    'version': '11.0',
+    'category': 'Sales',
+    'author': 'Arkana, Joenan <joenan@arkana.co.id>',
+    'website': 'https://www.arkana.co.id',
+    'description': """Custom Sale Management for Javara""",
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'test': [
+    ],
+    'installable': True,
+}
