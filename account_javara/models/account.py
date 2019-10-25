51dcaa9b8bc650a3563e2ecb81dac3d1bcc6b492
javaraid <33311666+javaraid@users.noreply.github.com>
2019-10-25 13:59:02 +0700

    Update account.py

--- account_javara/models/account.py
+++ account_javara/models/account.py
@@ -16,16 +16,16 @@ class AccountMoveLine(models.Model):
 
     # to add saleschannel in PoS transactions
     # used in reporting
-    @api.multi
-    @api.depends('partner_id.team_id', 'ref')
-    def _compute_saleschannel(self):
-        for record in self:
-            if record.partner_id and record.partner_id.team_id:
-                record.saleschannel_id = record.partner_id.team_id
-            elif not record.partner_id and record.journal_id.type == 'sale' and 'POS' in record.ref:
-                record.saleschannel_id = self.env['pos.session'].search([('name','=', record.ref)])[0].crm_team_id
-            else:
-                continue
+#     @api.multi
+#     @api.depends('partner_id.team_id', 'ref')
+#     def _compute_saleschannel(self):
+#         for record in self:
+#             if record.partner_id and record.partner_id.team_id:
+#                 record.saleschannel_id = record.partner_id.team_id
+#             elif not record.partner_id and record.journal_id.type == 'sale' and 'POS' in record.ref:
+#                 record.saleschannel_id = self.env['pos.session'].search([('name','=', record.ref)])[0].crm_team_id
+#             else:
+#                 continue
 
     @api.multi
     @api.depends('invoice_id', 'date_maturity', 'invoice_id.date_ttf', 'invoice_id.date_invoice')
