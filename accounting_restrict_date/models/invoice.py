4d2f8d72a0568756ec5ed013d4c85fba1f221616
javaraid <33311666+javaraid@users.noreply.github.com>
2020-02-11 19:40:30 +0700

    Update invoice.py
    fix should be str not datetime

--- accounting_restrict_date/models/invoice.py
+++ accounting_restrict_date/models/invoice.py
@@ -16,7 +16,7 @@ class AccountInvoice(models.Model):
         if not user.has_group('account.group_account_manager'):
             start_of_month = date.today().replace(day=1)
             date_to_use = self.date_invoice or datetime.today()
-            date_invoice = datetime.strptime(date_to_use, '%Y-%m-%d').replace(hour=0,minute=0,second=0).date()
+            date_invoice = datetime.strptime(str(date_to_use), '%Y-%m-%d').replace(hour=0,minute=0,second=0).date()
             if date_invoice < start_of_month:
                 raise UserError('Tidak boleh input dokumen bulan lalu! Hubungi Admin untuk info selengkapnya')
 
