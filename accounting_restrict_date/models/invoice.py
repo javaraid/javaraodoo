c78917273a4f738140c62d9a94fad979a9d106c5
javaraid <33311666+javaraid@users.noreply.github.com>
2020-02-11 19:43:27 +0700

    Update invoice.py

--- accounting_restrict_date/models/invoice.py
+++ accounting_restrict_date/models/invoice.py
@@ -15,8 +15,8 @@ class AccountInvoice(models.Model):
         user = self.env.user
         if not user.has_group('account.group_account_manager'):
             start_of_month = date.today().replace(day=1)
-            date_to_use = self.date_invoice or datetime.today()
-            date_invoice = datetime.strptime(str(date_to_use), '%Y-%m-%d').replace(hour=0,minute=0,second=0).date()
+            date_to_use = datetime.strptime(self.date_invoice, '%Y-%m-%d') or datetime.today()
+            date_invoice = date_to_use.replace(hour=0,minute=0,second=0).date()
             if date_invoice < start_of_month:
                 raise UserError('Tidak boleh input dokumen bulan lalu! Hubungi Admin untuk info selengkapnya')
 
