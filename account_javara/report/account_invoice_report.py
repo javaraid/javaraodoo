bb8121bebc33ded06f1a9559e51fab69ea6a9f15
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2020-03-09 11:40:13 +0700

    [FIX] account_javara: invoice analysis just sign for price with tax

--- account_javara/report/account_invoice_report.py
+++ account_javara/report/account_invoice_report.py
@@ -17,7 +17,7 @@ class AccountInvoiceReport(models.Model):
         return super(AccountInvoiceReport, self)._select() + ", sub.invoice_id as invoice_id, sub.price_with_tax as price_with_tax, sub.origin as origin"
 
     def _sub_select(self):
-        return super(AccountInvoiceReport, self)._sub_select() + ", ai.id AS invoice_id, SUM(ail.price_total) AS price_with_tax, ai.origin AS origin"
+        return super(AccountInvoiceReport, self)._sub_select() + ", ai.id AS invoice_id, SUM(invoice_type.sign) AS price_with_tax, ai.origin AS origin"
 
     def _group_by(self):
         return super(AccountInvoiceReport, self)._group_by() + ", ai.id, ai.origin"
