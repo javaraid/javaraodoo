c2f4bff74f1d8511b6c0b5a88cfc5de20d9d9ac4
fady <fady.noor@gmail.com>
2020-03-21 18:11:46 +0700

    [UPD] account_javara: fix sign of Total With Tax

--- account_javara/report/account_invoice_report.py
+++ account_javara/report/account_invoice_report.py
@@ -3,7 +3,6 @@
 from odoo import tools
 from odoo import models, fields, api
 
-
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
@@ -17,7 +16,7 @@ class AccountInvoiceReport(models.Model):
         return super(AccountInvoiceReport, self)._select() + ", sub.invoice_id as invoice_id, sub.price_with_tax as price_with_tax, sub.origin as origin"
 
     def _sub_select(self):
-        return super(AccountInvoiceReport, self)._sub_select() + ", ai.id AS invoice_id, SUM(invoice_type.sign) AS price_with_tax, ai.origin AS origin"
+        return super(AccountInvoiceReport, self)._sub_select() + ", ai.id AS invoice_id, SUM(ail.price_total_signed * invoice_type.sign) AS price_with_tax, ai.origin AS origin"
 
     def _group_by(self):
         return super(AccountInvoiceReport, self)._group_by() + ", ai.id, ai.origin"
