c6fad9d0eea192bc762d07f496e8fc12add2ed26
Erlangga <erlangga.ip@gmail.com>
2020-11-12 09:35:44 +0700

    [ADD] tada: init

---
+++ tada/__manifest__.py
@@ -0,0 +1,16 @@
+{'name': 'Tada Integration',
+'summary': 'Integrates customer, product, and order.',
+'version': '1.0',
+'author': 'Arkana Teknologi',
+'website': 'https://arkanateknologi.co.id',
+'category': 'Sales',
+'depends': ['sale_stock', 'web_widget_image_url'],
+'data': ['security/ir.model.access.csv',
+         'data/tada_data.xml',
+         'wizard/tada_authenticate_wizard.xml',
+         'views/tada_views.xml',
+         'views/tada_product_views.xml',
+         'views/tada_order_views.xml',
+         'views/menu.xml'
+         ]
+}
