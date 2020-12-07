7104a1f32855f793bdc92060d4b2276e7efc9926
Miftahussalam <miftah_salam@ymail.com>
2020-12-08 00:55:45 +0700

    sale_javara: bug fix division by zero

--- sale_javara/report/sale_report.py
+++ sale_javara/report/sale_report.py
@@ -24,9 +24,15 @@ class SaleReport(models.Model):
         """, 
         extract(epoch from age(s.delivered_at,s.confirmation_date))/(24*60*60)::decimal(16,2) as delay_real, 
         sum(l.amt_invoiced_untax / COALESCE(cr.rate, 1.0)) as amt_invoiced_untax,
-        SUM((l.amt_invoiced_untax / COALESCE(cr.rate, 1.0)) / (COALESCE(l.price_subtotal, 1.0) / COALESCE(cr.rate, 1.0)) * 100) as invoiced_amount_percentage,
-        sum((l.qty_invoiced / u.factor * u2.factor) / (COALESCE(l.product_uom_qty / u.factor * u2.factor, 1.0)) * 100) as invoiced_qty_percentage,
-        sum((l.qty_delivered / u.factor * u2.factor) / (COALESCE(l.product_uom_qty / u.factor * u2.factor, 1.0)) * 100) as delivered_qty_percentage,
+        SUM(CASE WHEN l.price_subtotal != 0
+            THEN (l.amt_invoiced_untax / COALESCE(cr.rate, 1.0)) / (COALESCE(l.price_subtotal, 1.0) / COALESCE(cr.rate, 1.0) * 100)
+            ELSE 0.0 END) AS invoiced_amount_percentage,
+        SUM(CASE WHEN l.product_uom_qty != 0
+            THEN (l.qty_invoiced / u.factor * u2.factor) / (COALESCE(l.product_uom_qty / u.factor * u2.factor, 1.0) * 100) 
+            ELSE 0.0 END) AS invoiced_qty_percentage,
+        SUM(CASE WHEN l.product_uom_qty != 0
+            THEN (l.qty_delivered / u.factor * u2.factor) / (COALESCE(l.product_uom_qty / u.factor * u2.factor, 1.0) * 100) 
+            ELSE 0.0 END) AS delivered_qty_percentage,
         sum((l.price_subtotal / COALESCE(cr.rate, 1.0)) + ((l.price_unit / COALESCE(cr.rate, 1.0)) * l.discount / 100) * (COALESCE(l.product_uom_qty / u.factor * u2.factor, 1.0))) as total_without_discount,
         sum(((l.price_unit / COALESCE(cr.rate, 1.0)) * l.discount / 100) * (COALESCE(l.product_uom_qty / u.factor * u2.factor, 1.0))) as total_discount
         """
