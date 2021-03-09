078a28d770634d3570cf4ab18b998846ec7d4ad4
Miftahussalam <miftah_salam@ymail.com>
2021-03-09 07:29:39 +0700

    security_base, security_advanced: groups access javara

--- security_advanced/__manifest__.py
+++ security_advanced/__manifest__.py
@@ -21,13 +21,18 @@
 
     # any module necessary for this one to work correctly
     'depends': [
+        'sale',
+        'purchase',
+        'account',
+        'stock',
+        'point_of_sale',
         'security_base',
     ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'views/views.xml',
+        'views/menuitem.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- security_base/__manifest__.py
+++ security_base/__manifest__.py
@@ -27,7 +27,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'views/views.xml',
+        'data/res_groups.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
